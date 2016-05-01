dd4490d7c8d8eb39fb12c00d0e4bf5bb2bcc97c8
Duong Dinh Cuong <cuongdd1@CuongQuay-Macbook.lan>
2016-05-02 00:19:18 +0700

    Start project with selenium nodes.

---
+++ selenium-function/selenium-function.js
@@ -0,0 +1,149 @@
+/**
+ * Copyright 2013,2015 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+    var webdriver = require("selenium-webdriver");
+    var util = require("util");
+    var vm = require("vm");
+
+    function sendResults(node,_msgid,msgs) {
+        if (msgs == null) {
+            return;
+        } else if (!util.isArray(msgs)) {
+            msgs = [msgs];
+        }
+        var msgCount = 0;
+        for (var m=0;m<msgs.length;m++) {
+            if (msgs[m]) {
+                if (util.isArray(msgs[m])) {
+                    for (var n=0; n < msgs[m].length; n++) {
+                        msgs[m][n]._msgid = _msgid;
+                        msgCount++;
+                    }
+                } else {
+                    msgs[m]._msgid = _msgid;
+                    msgCount++;
+                }
+            }
+        }
+        if (msgCount>0) {
+            node.send(msgs);
+        }
+    }
+
+    function FunctionNode(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        this.name = n.name;
+        this.func = n.func;
+        var functionText = "var results = null;"+
+                           "results = (function(msg){ "+
+                              "var __msgid__ = msg._msgid;"+
+                              "var node = {"+
+                                 "log:__node__.log,"+
+                                 "error:__node__.error,"+
+                                 "warn:__node__.warn,"+
+                                 "on:__node__.on,"+
+                                 "status:__node__.status,"+
+                                 "send:function(msgs){ __node__.send(__msgid__,msgs);}"+
+                              "};\n"+
+                              this.func+"\n"+
+                           "})(msg);";
+        this.topic = n.topic;
+        var sandbox = {
+            console:console,
+            util:util,
+            Buffer:Buffer,
+            __node__: {
+                log: function() {
+                    node.log.apply(node, arguments);
+                },
+                error: function() {
+                    node.error.apply(node, arguments);
+                },
+                warn: function() {
+                    node.warn.apply(node, arguments);
+                },
+                send: function(id, msgs) {
+                    sendResults(node, id, msgs);
+                },
+                on: function() {
+                    node.on.apply(node, arguments);
+                },
+                status: function() {
+                    node.status.apply(node, arguments);
+                }
+            },
+            context: {
+                global:RED.settings.functionGlobalContext || {}
+            },
+            webdriver: webdriver,
+            setTimeout: setTimeout,
+            clearTimeout: clearTimeout
+        };
+        var context = vm.createContext(sandbox);
+        try {
+            this.script = vm.createScript(functionText);
+            this.on("input", function(msg) {
+                try {
+                    var start = process.hrtime();
+                    context.msg = msg;
+                    this.script.runInContext(context);
+                    sendResults(this,msg._msgid,context.results);
+
+                    var duration = process.hrtime(start);
+                    var converted = Math.floor((duration[0] * 1e9 + duration[1])/10000)/100;
+                    this.metric("duration", msg, converted);
+                    if (process.env.NODE_RED_FUNCTION_TIME) {
+                        this.status({fill:"yellow",shape:"dot",text:""+converted});
+                    }
+                } catch(err) {
+
+                    var line = 0;
+                    var errorMessage;
+                    var stack = err.stack.split(/\r?\n/);
+                    if (stack.length > 0) {
+                        while (line < stack.length && stack[line].indexOf("ReferenceError") !== 0) {
+                            line++;
+                        }
+
+                        if (line < stack.length) {
+                            errorMessage = stack[line];
+                            var m = /:(\d+):(\d+)$/.exec(stack[line+1]);
+                            if (m) {
+                                var lineno = Number(m[1])-1;
+                                var cha = m[2];
+                                errorMessage += " (line "+lineno+", col "+cha+")";
+                            }
+                        }
+                    }
+                    if (!errorMessage) {
+                        errorMessage = err.toString();
+                    }
+                    this.error(errorMessage, msg);
+                }
+            });
+        } catch(err) {
+            // eg SyntaxError - which v8 doesn't include line number information
+            // so we can't do better than this
+            this.error(err);
+        }
+    }
+    RED.nodes.registerType("selenium",FunctionNode);
+    
+    RED.library.register("functions");
+};
---
+++ selenium-webdriver/selenium-webdriver.js
@@ -0,0 +1,340 @@
+/**
+ * Copyright 2013,2015 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+	"use strict";
+	var q = require('q');
+	var util = require("util");
+	var isReachable = require('is-reachable');
+	var webdriver = require("selenium-webdriver"),
+	    By = webdriver.By,
+	    until = webdriver.until;
+	var isUtf8 = require('is-utf8');
+
+	function SeleniumServerSetup(n) {
+		RED.nodes.createNode(this, n);
+
+		this.connected = false;
+		this.connecting = false;
+		this.usecount = 0;
+		// Config node state
+		this.remoteurl = n.remoteurl;
+		this.browser = n.browser;
+
+		var node = this;
+		this.register = function() {
+			node.usecount += 1;
+		};
+
+		this.deregister = function() {
+			node.usecount -= 1;
+			if (node.usecount == 0) {
+			}
+		};
+
+		this.connect = function() {
+			var deferred = q.defer();
+			if (!node.connected && !node.connecting) {
+				node.connecting = true;
+				var url = require('url').parse(node.remoteurl);
+				isReachable(url.host, function(error, reachable) {
+					if (!error && reachable) {
+						node.driver = new webdriver.Builder().forBrowser(node.browser).usingServer(node.remoteurl);
+						node.log(RED._("connected", {
+							server : (node.browser ? node.browser + "@" : "") + node.remoteurl
+						}));
+						node.connected = true;
+						node.emit('connected');
+						deferred.resolve(node.driver);
+					} else {
+						node.connecting = false;
+						node.connected = false;
+						deferred.reject({
+							Error : "Invalid configuration."
+						});
+					}
+				});
+			} else {
+				if (node.driver)
+					deferred.resolve(node.driver);
+			}
+			return deferred.promise;
+		};
+
+		this.on('close', function(closecomplete) {
+			if (this.connected) {
+				this.on('disconnected', function() {
+					closecomplete();
+				});
+				node.driver.quit();
+			} else {
+				closecomplete();
+			}
+		});
+	}
+
+
+	RED.nodes.registerType("selenium-server", SeleniumServerSetup);
+
+	function SeleniumOpenURLNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		this.server = n.server;
+		this.weburl = n.weburl;
+		this.webtitle = n.webtitle;
+		this.webtimeout = n.webtimeout;
+		this.serverObj = RED.nodes.getNode(this.server);
+		var node = this;
+		if (node.serverObj) {
+			node.serverObj.register();
+			node.serverObj.connect().then(function(webdriver) {
+				node.status({
+					fill : "green",
+					shape : "ring",
+					text : "common.status.connected"
+				});
+			}, function(error) {
+				node.status({
+					fill : "red",
+					shape : "ring",
+					text : "common.status.disconnected"
+				});
+			});
+		} else {
+			node.error(RED._("common.status.not-connected"));
+		}
+		this.on("input", function(msg) {
+			node.serverObj.connect().then(function(webdriver) {
+				var driver = webdriver.build();
+				driver.get(node.weburl);
+				if (node.webtitle) {
+					driver.wait(until.titleIs(node.webtitle), parseInt(node.webtimeout)).catch(function(errorback) {
+						node.status({
+							fill : "red",
+							shape : "ring",
+							text : "common.status.expected-failure"
+						});
+					}).then(function() {
+						driver.getTitle().then(function(title) {
+							node.send({
+								driver : driver,
+								payload : title
+							});
+						});
+					});
+				}
+				node.status({
+					fill : "green",
+					shape : "ring",
+					text : "common.status.connected"
+				});
+			}, function(error) {
+				node.status({
+					fill : "red",
+					shape : "ring",
+					text : "common.status.disconnected"
+				});
+			});
+		});
+		this.on('close', function() {
+			if (node.serverConn) {
+				node.serverObj.deregister();
+			}
+		});
+	}
+
+
+	RED.nodes.registerType("open-web", SeleniumOpenURLNode);
+
+	function SeleniumCloseBrowserNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.driver.quit();
+			node.send(msg);
+		});
+	}
+
+
+	RED.nodes.registerType("close-web", SeleniumCloseBrowserNode);
+
+	function SeleniumFindElementNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		this.selector = n.selector;
+		this.value = n.text;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.element = msg.driver.findElement(By[node.selector](node.value));
+			node.send(msg);
+		});
+	}
+
+	RED.nodes.registerType("find-object", SeleniumFindElementNode);
+	
+	function SeleniumSendKeysNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		this.keys = n.text;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.element.sendKeys(node.keys).then(function() {
+				node.send(msg);	
+			});
+		});
+	}
+
+	RED.nodes.registerType("send-keys", SeleniumSendKeysNode);
+
+	function SeleniumClickOnNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.element.click().then(function() {
+				node.send(msg);	
+			});
+		});
+	}
+
+
+	RED.nodes.registerType("click-on", SeleniumClickOnNode);
+	
+	function SeleniumSetValueNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		this.value = n.text;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.driver.executeScript("arguments[0].setAttribute('value', '" + node.value + "')", msg.element).then(function() {
+				node.send(msg);	
+			});
+		});
+	}
+
+	RED.nodes.registerType("set-value", SeleniumSetValueNode);
+
+	function SeleniumGetValueNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.element.getAttribute("value").then(function(text) {
+				msg.payload = text;
+				node.send(msg);	
+			});
+		});
+	}
+
+	RED.nodes.registerType("get-value", SeleniumGetValueNode);
+	
+	function SeleniumGetTextNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.element.getText().then(function(text) {
+				msg.payload = text;
+				node.send(msg);	
+			});
+			
+		});
+	}
+
+	RED.nodes.registerType("get-text", SeleniumGetTextNode);
+	
+	function SeleniumTakeScreenshotNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.element.takeScreenshot().then(function() {
+				node.send(msg);	
+			});
+		});
+	}
+	
+	RED.nodes.registerType("screenshot", SeleniumTakeScreenshotNode);
+	
+	function SeleniumNavToNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		this.url = n.url;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.driver.navigate().to(node.url).then(function() {
+				node.send(msg);	
+			});
+		});
+	}
+	
+	RED.nodes.registerType("nav-to", SeleniumNavToNode);
+	
+	function SeleniumNavBackNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.driver.navigate().back().then(function() {
+				node.send(msg);	
+			});
+		});
+	}
+	
+	RED.nodes.registerType("nav-back", SeleniumNavBackNode);
+	
+	function SeleniumNavForwardNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.driver.navigate().forward().then(function() {
+				node.send(msg);	
+			});
+		});
+	}
+	
+	RED.nodes.registerType("nav-forward", SeleniumNavForwardNode);
+	
+	function SeleniumNavRefreshNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.driver.navigate().refresh().then(function() {
+				node.send(msg);	
+			});
+		});
+	}
+	
+	RED.nodes.registerType("nav-refresh", SeleniumNavRefreshNode);
+	
+	function SeleniumRunScriptNode(n) {
+		RED.nodes.createNode(this, n);
+		this.name = n.name;
+		this.func = n.func;
+		var node = this;
+		this.on("input", function(msg) {
+			msg.driver.executeScript(node.func, msg.element).then(function(results) {
+				msg.payload = results;
+				node.send(msg);	
+			});
+		});
+	}
+	
+	RED.nodes.registerType("run-script", SeleniumRunScriptNode);
+};
