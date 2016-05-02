bcc2e9a2004c6a22e739c60353a2630f0ebf4a78
CuongQuay <cuongdd1@CuongQuay-Macbook.lan>
2016-05-02 22:45:44 +0700

    add demo image and update guideline

--- selenium-function/selenium-function.js
+++ selenium-function/selenium-function.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013,2015 IBM Corp.
+ * Author: DUONG Dinh Cuong, cuong3ihut@gmail.com.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
