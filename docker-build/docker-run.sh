fa45f113d93ca4e48e9f6f5713ffd68d70c4e4ac
CuongQuay <cuong3ihut@gmail.com>
2016-07-22 16:05:02 +0700

    Update flow

--- docker-build/docker-run.sh
+++ docker-build/docker-run.sh
@@ -1,4 +1,4 @@
 sh docker run -d -P --name selenium-hub selenium/hub
-sh docker run -d --link selenium-hub:hub selenium/node-chrome
-sh docker run -d --link selenium-hub:hub selenium/node-firefox
-sh docker run -d --link selenium-hub:hub cuongquay/node-red-selenium-webdriver
+sh docker run -d --name node-chrome --link selenium-hub:hub selenium/node-chrome
+sh docker run -d --name node-firefox --link selenium-hub:hub selenium/node-firefox
+sh docker run -d -P --name node-red --link selenium-hub:hub cuongquay/docker-node-red-selenium-webdriver
